b2cc55872f14c3f7586313ec8fa4ec9253b16058
Kenji Fukushima <kfuku52@gmail.com>
2021-05-21 11:25:44 +0200

    supported accession2fasta

---
+++ cdskit/accession2fasta.py
@@ -0,0 +1,53 @@
+from Bio import Entrez
+from Bio import SeqIO
+
+import sys
+import time
+
+from cdskit.util import *
+
+def accession2seq_record(accessions, database, batch_size=1000):
+    num_accession = len(accessions)
+    start_time = time.time()
+    seq_records = list()
+    for i in numpy.arange(numpy.ceil(num_accession//batch_size)+1):
+        start = int(i*batch_size)
+        end = int(((i+1)*batch_size)-1) if num_accession >= int(((i+1)*batch_size)-1) else num_accession
+        if not quiet:
+            sys.stderr.write('Retrieving accessions: {:,}-{:,}, {:,} [sec]\n'.format(start, end, int(time.time()-start_time)))
+        handle = Bio.Entrez.efetch(db=database, id=accessions[start:end], rettype="gb", retmode="text", retmax=batch_size)
+        seq_records += list(Bio.SeqIO.parse(handle, 'gb'))
+    if not quiet:
+        sys.stderr.write('Number of input accessions: {:,}\n'.format(num_accession))
+        sys.stderr.write('Number of retrieved records {:,}\n'.format(len(seq_records)))
+    if (num_accession!=len(seq_records)):
+        missing_ids = []
+        for accession in accessions:
+            flag_found = False
+            for seq_record in seq_records:
+                if (accession==seq_record.id):
+                    flag_found = True
+                    break
+            if not flag_found:
+                missing_ids.append(accession)
+        if not quiet:
+            sys.stderr.write('Accessions failed to retrieve: {}\n'.format(' '.join(missing_ids)))
+    elapsed_time = int(time.time() - start_time)
+    if not quiet:
+        sys.stderr.write("Elapsed_time for sequence record retrieval: {:,} [sec]\n".format(elapsed_time))
+    return seq_records
+
+def accession2fasta_main(args):
+    if args.email!='':
+        Bio.Entrez.email = args.email
+    accessions = read_item_per_line_file(file=args.accession_file)
+    records = accession2seq_record(accessions, args.ncbi_database)
+    for i in range(len(records)):
+        records[i].name = ''
+        records[i].description = ''
+        records[i].id = get_seqname(records[i], seqnamefmt=args.seqnamefmt)
+        if args.extract_cds:
+            records[i] = replace_seq2cds(record=records[i])
+    write_seqs(records, args)
+    if not args.quiet:
+        sys.stderr.write('cdskit accession2fasta: end\n')
--- cdskit/util.py
+++ cdskit/util.py
@@ -1,7 +1,9 @@
-import sys
 import Bio.Seq
 import Bio.SeqIO
 import numpy
+
+import re
+import sys
 
 def read_seqs(seqfile, seqformat, quiet):
     if seqfile=='-':
@@ -10,12 +12,12 @@ def read_seqs(seqfile, seqformat, quiet):
         parsed = seqfile
     records = list(Bio.SeqIO.parse(parsed, seqformat))
     if not quiet:
-        sys.stderr.write('number of input sequences: {}\n'.format(len(records)))
+        sys.stderr.write('Number of input sequences: {}\n'.format(len(records)))
     return records
 
 def write_seqs(records, args):
     if not args.quiet:
-        sys.stderr.write('number of output sequences: {}\n'.format(len(records)))
+        sys.stderr.write('Number of output sequences: {}\n'.format(len(records)))
     if args.outfile=='-':
         Bio.SeqIO.write(records, sys.stdout, args.outseqformat)
     else:
@@ -42,3 +44,39 @@ def records2array(records):
         seqchars.append(list(record.seq))
     arr = numpy.array(seqchars)
     return arr
+
+def read_item_per_line_file(file):
+    with open(file, 'r') as f:
+        out = f.read().split('\n')
+    out = [ o for o in out if o!='' ]
+    return out
+
+def get_seqname(record, seqnamefmt):
+    name_items = seqnamefmt.split('_')
+    seqname = ''
+    for name_item in name_items:
+        try:
+            new_name = record.annotations[name_item]
+            if type(new_name) is list:
+                new_name = new_name[0]
+            seqname += '_'+new_name
+        except:
+            available_items = ', '.join(list(record.annotations.keys()))
+            txt = 'Invalid --seqnamefmt element ({}) in {}. Available elements: {}'
+            raise Exception(txt.format(name_item, record.id, available_items))
+    seqname = re.sub('^_', '', seqname)
+    seqname = re.sub(' ', '_', seqname)
+    return seqname
+
+def replace_seq2cds(record):
+    flag_no_cds = True
+    for feature in record.features:
+        if feature.type=="CDS":
+            seq = feature.location.extract(record).seq
+            record.seq = seq
+            flag_no_cds = False
+            break
+    if flag_no_cds:
+        txt = 'Removed from output. No CDS found in: {}\n'
+        os.stderr.write(txt.format(record.id))
+    return record
