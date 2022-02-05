c8c48ecc0a8591bfd57180c5395207963c4fecdf
Kenji Fukushima <kfuku52@gmail.com>
2022-02-05 19:02:52 +0100

    rmseq

---
+++ cdskit/rmseq.py
@@ -0,0 +1,27 @@
+import sys
+import re
+from cdskit.util import *
+
+def rmseq_main(args):
+    if not args.quiet:
+        sys.stderr.write('cdskit rmseq: start\n')
+    if (args.verbose)&(not args.quiet):
+        sys.stderr.write(str(args)+'\n')
+    records = read_seqs(seqfile=args.seqfile, seqformat=args.inseqformat, quiet=args.quiet)
+    new_records = list()
+    for record in records:
+        flag = True
+        if re.fullmatch(args.seqname, record.name):
+            flag = False
+        if args.problematic_percent>0:
+            num_problematic_char = 0
+            for pc in args.problematic_char:
+                num_problematic_char += record.seq.count(pc)
+            rate_problematic = num_problematic_char / len(record.seq)
+            if rate_problematic >= (args.problematic_percent/100):
+                flag = False
+        if flag:
+            new_records.append(record)
+    write_seqs(new_records, args)
+    if not args.quiet:
+        sys.stderr.write('cdskit rmseq: end\n')
