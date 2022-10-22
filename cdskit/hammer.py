9566c02c133adb2b16e77269c71fffdf7153030e
Kenji Fukushima <kfuku52@gmail.com>
2022-10-22 15:39:09 +0200

    hammer --prevent_gap_only yes

--- cdskit/hammer.py
+++ cdskit/hammer.py
@@ -6,26 +6,42 @@ def hammer_main(args):
         sys.stderr.write('cdskit hammer: start\n')
     if (args.verbose)&(not args.quiet):
         sys.stderr.write(str(args)+'\n')
-    records = read_seqs(seqfile=args.seqfile, seqformat=args.inseqformat, quiet=args.quiet)
-    max_len = max([ len(r.seq) for r in records ])//3
-    missing_site = numpy.zeros(shape=[max_len,], dtype=int)
-    for record in records:
-        aaseq = str(record.seq.translate(table=args.codontable, to_stop=False, gap="-"))
-        for i in numpy.arange(len(aaseq)):
-            if aaseq[i] in ['-','?','X','*']:
-                missing_site[i] += 1
-        if len(aaseq)<missing_site.shape[0]:
-            missing_site[len(aaseq):] += 1
-    non_missing_site = len(records) - missing_site
-    non_missing_idx = numpy.argwhere(non_missing_site>=args.nail)
-    non_missing_idx = numpy.reshape(non_missing_idx, newshape=[non_missing_idx.shape[0],])
-    num_removed_site = max_len - non_missing_idx.shape[0]
-    if not args.quiet:
-        sys.stderr.write('{:,} out of {:,} codon sites will be removed.\n'.format(num_removed_site, max_len))
-    for record in records:
-        seq = str(record.seq)
-        new_seq = ''.join([ seq[nmi*3:nmi*3+3] for nmi in non_missing_idx ])
-        record.seq = Bio.Seq.Seq(new_seq)
+    for current_nail in numpy.flip(numpy.arange(4)+1):
+        records = read_seqs(seqfile=args.seqfile, seqformat=args.inseqformat, quiet=args.quiet)
+        max_len = max([ len(r.seq) for r in records ])//3
+        missing_site = numpy.zeros(shape=[max_len,], dtype=int)
+        for record in records:
+            aaseq = str(record.seq.translate(table=args.codontable, to_stop=False, gap="-"))
+            for i in numpy.arange(len(aaseq)):
+                if aaseq[i] in ['-','?','X','*']:
+                    missing_site[i] += 1
+            if len(aaseq)<missing_site.shape[0]:
+                missing_site[len(aaseq):] += 1
+        non_missing_site = len(records) - missing_site
+        non_missing_idx = numpy.argwhere(non_missing_site>=current_nail)
+        non_missing_idx = numpy.reshape(non_missing_idx, newshape=[non_missing_idx.shape[0],])
+        num_removed_site = max_len - non_missing_idx.shape[0]
+        if not args.quiet:
+            sys.stderr.write('{:,} out of {:,} codon sites will be removed.\n'.format(num_removed_site, max_len))
+        for record in records:
+            seq = str(record.seq)
+            new_seq = ''.join([ seq[nmi*3:nmi*3+3] for nmi in non_missing_idx ])
+            record.seq = Bio.Seq.Seq(new_seq)
+        if args.prevent_gap_only:
+            flag_gap_only = False
+            for record in records:
+                seq_len = len(record.seq)
+                num_hyphen = record.seq.count('-')
+                num_N = record.seq.count('N')
+                num_X = record.seq.count('X')
+                num_gap = num_hyphen + num_N + num_X
+                if seq_len==num_gap:
+                    flag_gap_only = True
+                    txt = 'A gap-only sequence was generated with --nail {}. Will try --nail {}: {}\n'
+                    sys.stderr.write(txt.format(current_nail, current_nail-1, record.name))
+            if flag_gap_only:
+                continue
+        break
     write_seqs(records, args)
     if not args.quiet:
         sys.stderr.write('cdskit hammer: end\n')
