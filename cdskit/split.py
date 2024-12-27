ee77214ce3484695a63bc29d97367f61d9e873ba
Kenji Fukushima <kfuku52@gmail.com>
2024-12-27 22:09:49 +0900

    supported --prefix

--- cdskit/split.py
+++ cdskit/split.py
@@ -12,9 +12,13 @@ def split_main(args):
         first_records[i].seq = records[i].seq[0::3]
         second_records[i].seq = records[i].seq[1::3]
         third_records[i].seq = records[i].seq[2::3]
+    if args.prefix=='INFILE':
+        prefix_str = args.seqfile
+    else:
+        prefix_str = args.prefix
     sys.stderr.write(f'Writing first codon positions.\n')
-    write_seqs(records=first_records, outfile=f'1st_codon_positions.{args.outseqformat}', outseqformat=args.outseqformat)
+    write_seqs(records=first_records, outfile=f'{prefix_str}_1st_codon_positions.{args.outseqformat}', outseqformat=args.outseqformat)
     sys.stderr.write(f'Writing second codon positions.\n')
-    write_seqs(records=second_records, outfile=f'2nd_codon_positions.{args.outseqformat}', outseqformat=args.outseqformat)
+    write_seqs(records=second_records, outfile=f'{prefix_str}_2nd_codon_positions.{args.outseqformat}', outseqformat=args.outseqformat)
     sys.stderr.write(f'Writing third codon positions.\n')
-    write_seqs(records=third_records, outfile=f'3rd_codon_positions.{args.outseqformat}', outseqformat=args.outseqformat)
+    write_seqs(records=third_records, outfile=f'{prefix_str}_3rd_codon_positions.{args.outseqformat}', outseqformat=args.outseqformat)
